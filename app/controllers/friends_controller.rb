360d5bf5d18def218d3e91d2557008069f791274
Nirbhay Sibal <sibal.nirbhaytlc@gmail.com>
2020-12-14 15:42:56 -0600

    add relationships between friends and user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,28 +1,20 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
 
-  # GET /friends
-  # GET /friends.json
   def index
     @friends = Friend.all
   end
 
-  # GET /friends/1
-  # GET /friends/1.json
   def show
   end
 
-  # GET /friends/new
   def new
     @friend = Friend.new
   end
 
-  # GET /friends/1/edit
   def edit
   end
 
-  # POST /friends
-  # POST /friends.json
   def create
     @friend = Friend.new(friend_params)
 
@@ -37,8 +29,6 @@ class FriendsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /friends/1
-  # PATCH/PUT /friends/1.json
   def update
     respond_to do |format|
       if @friend.update(friend_params)
@@ -51,8 +41,6 @@ class FriendsController < ApplicationController
     end
   end
 
-  # DELETE /friends/1
-  # DELETE /friends/1.json
   def destroy
     @friend.destroy
     respond_to do |format|
@@ -62,13 +50,12 @@ class FriendsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_friend
-      @friend = Friend.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
-    end
+  def set_friend
+    @friend = Friend.find(params[:id])
+  end
+
+  def friend_params
+    params.require(:friend).permit(:user_id, :first_name, :last_name, :email, :phone, :twitter)
+  end
 end
